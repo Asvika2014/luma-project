3710894d686357bbb1f50c489faa0af408183f2c
Asvika2014 <a.sand#0@gmail.com>
2024-07-10 11:23:07 +0530

    Adding comments

--- Selenium_Mini_Project.java
+++ Selenium_Mini_Project.java
@@ -20,7 +20,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
-/**/
+/*This is my class*/
 public class Selenium_Mini_Project {
 	
 	
